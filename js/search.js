a9a20ecdd42c569e963b12090012068ac25ea37b
MassaHex <105466578+MassaHex@users.noreply.github.com>
2023-01-17 22:08:11 -0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -21,7 +21,7 @@ function startSearch() {
   ul = document.getElementById("search");
   li = ul.getElementsByTagName("a");
   for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("h1")[0];
+    a = li[i].getElementsByTagName("p")[0];
     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
     } else {
